3c4f2d675b8949a01cbf2c6a6af609840b3eb22d
Jasper Brandsma <brandjas@users.noreply.github.com>
2024-08-28 10:40:28 +0200

    Remove sound.enable as it has been removed in nixos unstable

--- configuration.nix
+++ configuration.nix
@@ -109,7 +109,6 @@ let displayManager = if machineOptions.desktop == "gnome" then "gdm" else "sddm"
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
